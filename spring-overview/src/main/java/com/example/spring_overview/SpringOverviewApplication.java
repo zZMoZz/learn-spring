2355c9db0be2448b339f356b1201c7c9218c0029
zmuhsenz <zmohamedmohsnz@gmail.com>
2024-09-22 16:16:51 +0300

    prepare spring-overview

---
+++ spring-overview/src/main/java/com/example/spring_overview/SpringOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOverviewApplication.class, args);
+	}
+
+}
---
+++ spring-overview/src/test/java/com/example/spring_overview/SpringOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_overview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
