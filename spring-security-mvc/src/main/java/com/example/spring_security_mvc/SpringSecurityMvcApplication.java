8de9bba847dff19c5c6e5d16afeb914ddbc3e347
zmuhsenz <zmohamedmohsnz@gmail.com>
2024-10-22 02:20:47 +0300

    prepare spring-security-mvc

---
+++ spring-security-mvc/src/main/java/com/example/spring_security_mvc/SpringSecurityMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMvcApplication.class, args);
+	}
+
+}
