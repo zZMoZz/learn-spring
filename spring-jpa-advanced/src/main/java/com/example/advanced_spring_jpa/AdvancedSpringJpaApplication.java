4bc3df3771503914d9573aa5e85630371b5d02b0
zmuhsenz <zmohamedmohsnz@gmail.com>
2024-10-31 11:42:50 +0300

    prepare spring-jpa-advanced

---
+++ spring-jpa-advanced/src/main/java/com/example/advanced_spring_jpa/AdvancedSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanced_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedSpringJpaApplication.class, args);
+	}
+
+}
---
+++ spring-jpa-advanced/src/test/java/com/example/advanced_spring_jpa/AdvancedSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advanced_spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
